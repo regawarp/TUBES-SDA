c800ac4ee09863616f44974c8c02da0d28b548d4
naufala <ahmadajinaufalali@gmail.com>
2018-07-05 01:17:07 +0700

    delete unnecesary and add report

--- ultilites.h
+++
@@ -1,9 +0,0 @@
-#include "strukturdata.h"
-
-void MakeTreeStatistik(TreeStatistik *tree, FILE *file, int jumlahFile, int urutanFile);
-void stopwordRemoval(char *kata);
-void createStopwordTree();
-void createIrregularVerbTree();
-void showMenu();
-void substr(char dest[], char source[], int pos, int length);
-
